8ce4ff7c936a93a39acb53275a8655ff473d5bac
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2022-10-28 15:03:43 +0200

    Fix audio source

--- res/js/app.js
+++ res/js/app.js
@@ -1,6 +1,6 @@
 let loader = document.getElementById('loader')
 let btn = loader.getElementsByClassName('btn')[0]
-let audio = new Audio(""); // On crée une instance audio (ne pas oublier à changer la source de la musique)
+let audio = new Audio("https://cdn.discordapp.com/attachments/669876482742681600/1033017844771856404/Team_Fortress_2_Soundtrack_Main_Theme.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
 btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
 function loadPage() {
   loader.style.opacity = 0
