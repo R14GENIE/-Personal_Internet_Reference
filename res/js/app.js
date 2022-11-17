17793393c4b161bfa7b3147fc443a2b4e10c9ffc
R14GENIE <ruben4961.g@gmail.com>
2022-11-17 16:19:38 +0100

    A

--- res/js/app.js
+++ res/js/app.js
@@ -1,6 +1,6 @@
 let loader = document.getElementById('loader')
 let btn = loader.getElementsByClassName('btn')[0]
-let audio = new Audio(""); // On crée une instance audio (ne pas oublier à changer la source de la musique)
+let audio = new Audio("res/audio/TF2MainTheme.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
 btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
 function loadPage() {
   loader.style.opacity = 0
