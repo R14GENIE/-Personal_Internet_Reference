98e0032dbf25696ec7dd94f0ed876816a9725a91
R14GENIE <ruben4961.g@gmail.com>
2022-11-21 18:05:43 +0100

    I add song

--- res/js/Meet.js
+++ res/js/Meet.js
@@ -1,18 +1,8 @@
 let loader = document.getElementById('loader')
 let btn = loader.getElementsByClassName('btn')[0]
-let audio = new Audio("res/audio/MeetAudio.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
+let audio = new Audio("../../res/audio/MeetAudio.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
+loader.style.opacity = 1
 audio.play()
-setTimeout(loadPage, 3000);
-function loadPage() {
-  loader.style.opacity = 0
-}
-/*
-// Une fois que le loader est invisible on le display none pour ne plus avoir aucune interaction avec
-loader.addEventListener('transitionend', () => { 
-  loader.style.display = "none"
-  
-  // on jour l'audio 
-  audio.play()
-})
-*/
+wait(4000)
+loader.style.opacity = 0
  
