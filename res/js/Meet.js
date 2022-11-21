a2a18ec8b142d195b62651798931a7f9bf1930f7
R14GENIE <ruben4961.g@gmail.com>
2022-11-21 18:24:53 +0100

    Sandvich

--- res/js/Meet.js
+++ res/js/Meet.js
@@ -1,8 +1,11 @@
-let loader = document.getElementById('loader')
+let loader = document.getElementById('loaderMeet')
 let btn = loader.getElementsByClassName('btn')[0]
 let audio = new Audio("../../res/audio/MeetAudio.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
-loader.style.opacity = 1
-audio.play()
-wait(4000)
 loader.style.opacity = 0
+//audio.play()
+audio.addEventListener('ended', (e) => {
+  loader.style.opacity = 0
+  console.log('audio fini')
+})
+
  
