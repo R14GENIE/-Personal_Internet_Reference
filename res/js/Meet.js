75ead0bee5b35b1171e70219d3eeaeb921c6e70c
R14GENIE <ruben4961.g@gmail.com>
2022-11-23 10:03:23 +0100

    Funny

--- res/js/Meet.js
+++ res/js/Meet.js
@@ -1,19 +1,25 @@
-let loader = document.getElementById('loaderMeet')
-let btn = loader.getElementsByClassName('btn')[0]
+let loaderButton = document.getElementById('buttonMeet')
+let meetPage = document.getElementById('imageMeet')
+let btn = loaderButton.getElementsByClassName('btn')[0]
 let audio = new Audio("../../res/audio/MeetAudio.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
-
+//loaderButton.style.opacity = 1
 //audio.play()
+loaderButton.style.opacity = 0
+meetPage.style.opacity = 0
+loaderButton.style.display = "none"
+meetPage.style.display = "none"
 btn.addEventListener('click', launchMusic) // Lors du click on appelle la fonction loadPage qui met l'element invisible
-function loadPage() {
-  loader.style.opacity = 0
-}
+
 function launchMusic() {
+  loaderButton.style.opacity = 0
+  loaderButton.style.display = "none"
   audio.play()
 }
 audio.addEventListener('ended', (e) => {
-  loader.style.opacity = 0
+  meetPage.style.opacity = 0
+  meetPage.style.display = "none"
 })
-loader.addEventListener('transitionend', () => { 
-  loader.style.display = "none"
-})
+
+
+
  
