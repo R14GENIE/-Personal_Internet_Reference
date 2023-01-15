593bc23ce54f81885c5bb658288e8e755de4225a
R14GENIE <69074781+R14GENIE@users.noreply.github.com>
2023-01-15 10:30:06 +0100

    MinosPage

--- res/js/app.js
+++ res/js/app.js
@@ -1,16 +1,16 @@
 let loader = document.getElementById('loader')
 let btn = loader.getElementsByClassName('btn')[0]
-let audio = new Audio("res/audio/TF2MainTheme.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
+let audio = new Audio("res/audio/MusiqueMain.mp3"); // On crée une instance audio (ne pas oublier à changer la source de la musique)
 btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
 function loadPage() {
   loader.style.opacity = 0
 }
-loader.style.opacity = 1
+loader.style.opacity = 0
 // Une fois que le loader est invisible on le display none pour ne plus avoir aucune interaction avec
 loader.addEventListener('transitionend', () => { 
   loader.style.display = "none"
   
   // on jour l'audio 
-  audio.play()
+  //audio.play()
 })
 
