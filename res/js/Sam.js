5e232a499c9aaab0d6c07fd4e65469446f453cbf
R14GENIE <ruben4961.g@gmail.com>
2023-01-16 17:52:25 +0100

    Sam

---
+++ res/js/Sam.js
@@ -0,0 +1,22 @@
+const video = document.querySelector('video');
+let videoIntro = document.getElementById('videoIntro');
+let audio = new Audio("../../res/audio/ORDER.mp3");
+
+//audio.play()
+  audio.loop = true
+  audio.volume = 0.2
+  videoIntro.style.opacity = 1
+  videoIntro.style.display = "none"
+/*
+video.autoplay = true ;
+video.volume = 0.2;
+
+video.onended = (Event) => {
+  audio.play()
+  audio.loop = true
+  audio.volume = 0.2
+  videoIntro.style.opacity = 1
+  videoIntro.style.display = "none"
+}
+
+*/
