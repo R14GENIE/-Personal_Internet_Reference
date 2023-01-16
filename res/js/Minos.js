253f86a6a9777062b0b3b20778687faff5cb30ad
R14GENIE <ruben4961.g@gmail.com>
2023-01-16 18:50:43 +0100

    Sam page done

--- res/js/Minos.js
+++ res/js/Minos.js
@@ -2,6 +2,14 @@ const video = document.querySelector('video');
 let videoIntro = document.getElementById('videoIntro');
 let audio = new Audio("../../res/audio/ORDER.mp3");
 
+let loader = document.getElementById('videoIntro')
+let btn = loader.getElementsByClassName('btn')[0]
+btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
+function loadPage() {
+  video.currentTime = video.duration
+
+}
+
 video.autoplay = true ;
 video.volume = 0.2;
 
--- res/js/Sam.js
+++ res/js/Sam.js
@@ -1,13 +1,15 @@
 const video = document.querySelector('video');
 let videoIntro = document.getElementById('videoIntro');
-let audio = new Audio("../../res/audio/ORDER.mp3");
+let audio = new Audio("../../res/audio/TheonlyFear.mp3");
 
-//audio.play()
-  audio.loop = true
-  audio.volume = 0.2
-  videoIntro.style.opacity = 1
-  videoIntro.style.display = "none"
-/*
+let loader = document.getElementById('videoIntro')
+let btn = loader.getElementsByClassName('btn')[0]
+btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
+function loadPage() {
+  video.currentTime = video.duration
+
+}
+
 video.autoplay = true ;
 video.volume = 0.2;
 
@@ -19,4 +21,3 @@ video.onended = (Event) => {
   videoIntro.style.display = "none"
 }
 
-*/
