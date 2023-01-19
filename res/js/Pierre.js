b3c7f0b8392e81f0b2040b0298164baeae9d8cca
R14GENIE <ruben4961.g@gmail.com>
2023-01-19 08:52:11 +0100

    Add "Pierre"

---
+++ res/js/Pierre.js
@@ -0,0 +1,23 @@
+const video = document.querySelector('video');
+let videoIntro = document.getElementById('videoIntro');
+let audio = new Audio("../../res/audio/Pierre.mp3");
+
+let loader = document.getElementById('videoIntro')
+let btn = loader.getElementsByClassName('btn')[0]
+btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
+function loadPage() {
+  video.currentTime = video.duration
+
+}
+
+video.autoplay = true ;
+video.volume = 0.2;
+
+video.onended = (Event) => {
+  audio.play()
+  audio.loop = true
+  audio.volume = 0.2
+  videoIntro.style.opacity = 1
+  videoIntro.style.display = "none"
+}
+
