f82f6e957fd1f10b979a05d098e75995eee05c5b
R14GENIE <ruben4961.g@gmail.com>
2023-01-20 16:45:51 +0100

    Doom more

--- res/js/Doom.js
+++ res/js/Doom.js
@@ -1,32 +1,57 @@
 const video = document.querySelector('video');
 let videoIntro = document.getElementById('videoIntro');
-let audio = new Audio("../../res/audio/Mick Gordon  The Only Thing They Fear Is You  DOOM Eternal OST High Quality.mp3");
+let audioA = new Audio("../../res/audio/Mick Gordon  The Only Thing They Fear Is You  DOOM Eternal OST High Quality.mp3");
+let audioB = new Audio("../../res/audio/Doom OST - E1M1 - At Doom's Gate.mp3");
+
 let doomModValue = false;
 
 let loader = document.getElementById('videoIntro')
 let btn = loader.getElementsByClassName('btn')[0]
-let btnChange = document.getElementsByClassName('btn2')[0]
+let btnChange = document.getElementsByClassName('pictureClassBTN')[0]
+let imageLeft = document.getElementsByClassName('pictureClass')[0]
 btn.addEventListener('click', loadPage) // Lors du click on appelle la fonction loadPage qui met l'element invisible
 btnChange.addEventListener('click', doomMod) 
 
 function loadPage() {
   video.currentTime = video.duration
 }
+
 function doomMod() {
-  console.log("a")
+  if (!doomModValue) {
+    audioA.pause();
+    audioA.currentTime = 0;
+    audioB.play()
+    //Changement du site
+    btnChange.src = "../../data/image/logoMain/Doom.png"
+    imageLeft.src = "../../data/image/Doom/OldDoomSlayer.png"
+    document.getElementById("middleAutre").style.backgroundImage = "url('../../data/image/Doom/OldDoomBackground.jpg')";
+  }
+  else{
+    audioB.pause();
+    audioB.currentTime = 0;
+    audioA.play()
+    //Changement du site
+    btnChange.src = "../../data/image/logoMain/OldDoom.png"
+    imageLeft.src = "../../data/image/Doom/Doomguy_Eternal.png"
+    document.getElementById("middleAutre").style.backgroundImage = "../../data/image/Doom/OldDoomBackground.jpg";
+  }
+  doomModValue = !doomModValue;
 }
+
+audioA.loop = true
+audioA.volume = 0.1
+audioB.volume = 0.1
+//video.autoplay = true ;
+//video.volume = 0.2;
 videoIntro.style.opacity = 1
-  videoIntro.style.display = "none"
-/*
-video.autoplay = true ;
-video.volume = 0.2;
+videoIntro.style.display = "none"
+//audioA.play()
 
 video.onended = (Event) => {
-  audio.play()
-  audio.loop = true
-  audio.volume = 0.2
+  audioA.play()
+  audioA.loop = true
+  audioA.volume = 0.1
   videoIntro.style.opacity = 1
   videoIntro.style.display = "none"
 }
 
-*/
